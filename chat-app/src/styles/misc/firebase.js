99af7691eae39b949e77403c924ba89f92a9873a
Manasa <manasa1225@gmail.com>
2021-07-27 23:35:11 +0530

    initialized firebase project

---
+++ chat-app/src/styles/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCDyz9_IY8xpnyVNFxE2pTCclj1sv3X_bo',
+  authDomain: 'chat-web-app-d4e2e.firebaseapp.com',
+  projectId: 'chat-web-app-d4e2e',
+  storageBucket: 'chat-web-app-d4e2e.appspot.com',
+  messagingSenderId: '279748193182',
+  appId: '1:279748193182:web:3aa5e2f91c6d33af3f848f',
+};
+
+const app = firebase.initializeApp(config);
