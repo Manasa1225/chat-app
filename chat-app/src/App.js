381b3e93cd15e0ddb7a820dfbe545fead839fe4a
Manasa <manasa1225@gmail.com>
2021-07-29 18:11:04 +0530

    created public and private routes

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute>
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
